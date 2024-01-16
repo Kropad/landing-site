a43d5b4e10bce1d2cb693bed6305f9d6e5439e8c
Berzan <berzan@tutanota.com>
2024-01-16 10:43:58 +0000

    define: `Footer` component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="flex justify-center border-t border-neutral-200 bg-white">
+	<div class="flex w-full max-w-7xl flex-col px-4 py-12">
+		<p class="text-xl font-extrabold">© Kropad</p>
+	</div>
+</footer>
