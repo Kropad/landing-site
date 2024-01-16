437cca5336c35c746ea26d70f5dbce158cf1cff7
Berzan <berzan@tutanota.com>
2024-01-16 10:44:12 +0000

    define: `Menu` component

---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { IconClose } from "$lib/icons/Close"
+	import { isMenuOpen } from "$lib/stores/isMenuOpen"
+	import { slide } from "svelte/transition"
+</script>
+
+{#if $isMenuOpen}
+	<menu
+		transition:slide={{ duration: 200, delay: 200 }}
+		class="fixed top-0 z-50 flex h-full w-full flex-col border-b border-neutral-200 bg-white md:hidden"
+	>
+		<div class="h-13 flex items-center justify-end px-4">
+			<button on:click={isMenuOpen.close}>
+				{@html IconClose()}
+			</button>
+		</div>
+		<nav class="flex flex-col gap-6 px-10 py-1 text-3xl font-bold">
+			<a href="/" on:click={isMenuOpen.toggle}> Homepage </a>
+			<a href="https://discord.com" target="_blank" on:click={isMenuOpen.toggle}> Discord </a>
+			<a href="https://twitter.com/kropadcom" target="_blank" on:click={isMenuOpen.toggle}>
+				X / Twitter
+			</a>
+			<a href="https://t.me/kropad" target="_blank" on:click={isMenuOpen.toggle}> Telegram </a>
+			<a href="https://github.com/Kropad" target="_blank" on:click={isMenuOpen.toggle}> GitHub </a>
+			<a href="mailto:contact@kropad.com" target="_blank" on:click={isMenuOpen.toggle}> Mail </a>
+		</nav>
+	</menu>
+{/if}
