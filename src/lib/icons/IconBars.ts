fab9f9ef2f7b5937bf36b9a29e98d0daa6198f8d
Berzan <berzan@tutanota.com>
2024-01-16 10:46:18 +0000

    define: Bars icon

---
+++ src/lib/icons/IconBars.ts
@@ -0,0 +1,2 @@
+export const IconBars = () =>
+	`<svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" style="--sl-icon-size: 1.5rem;"><path d="M3 8h18a1 1 0 1 0 0-2H3a1 1 0 0 0 0 2Zm18 8H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2Zm0-5H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2Z"></path></svg>`
