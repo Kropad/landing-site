0edaf930eee1447cec78337d98a1aaa10a26feab
Berzan <berzan@tutanota.com>
2024-01-16 10:47:37 +0000

    format: code style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
+<script lang="ts">
+	import { page } from "$app/stores"
+	import Header from "$lib/components/Header.svelte"
+	import Footer from "$lib/components/Footer.svelte"
+	import Seo from "$lib/components/Seo.svelte"
+	import "../app.css"
+	import Menu from "$lib/components/Menu.svelte"
+</script>
+
 <Seo
 	data={{
 		title: $page.data.title,
 		description: $page.data.description,
 	}}
 />
-<slot />
 
-<script lang="ts">
-	import { page } from "$app/stores"
-	import Seo from "$lib/components/Seo.svelte"
-	import "../app.css"
-</script>
+<Header />
+<Menu />
+<slot />
+<Footer />
