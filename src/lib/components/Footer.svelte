283325d21e3293b020b7317f3efce897fcf8ca8d
Berzan <berzan@tutanota.com>
2024-01-17 17:25:36 +0000

    update: content

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,54 @@
 <footer class="flex justify-center border-t border-neutral-200 bg-white">
-	<div class="flex w-full max-w-7xl flex-col px-4 py-12">
-		<p class="text-xl font-extrabold">© Kropad</p>
+	<div
+		class="flex w-full max-w-7xl flex-col gap-16 px-8 py-16 pt-20 sm:flex-row sm:justify-between"
+	>
+		<p class="text-xl font-extrabold">© Kropad ~ 2024</p>
+		<div class="flex">
+			<div class="flex flex-col gap-1">
+				<p class="text-lg font-extrabold">Social Links</p>
+				<div class="flex flex-col gap-0.5 text-sm text-neutral-900/60">
+					<a
+						href="https://discord.com"
+						class="hover:text-green-600 hover:underline"
+						aria-label="Kropad's Discord server"
+						target="_blank"
+					>
+						Discord
+					</a>
+					<a
+						href="https://twitter.com/kropadcom"
+						class="hover:text-green-600 hover:underline"
+						aria-label="Kropad's Twitter profile"
+						target="_blank"
+					>
+						X / Twitter
+					</a>
+					<a
+						href="https://t.me/kropad"
+						class="hover:text-green-600 hover:underline"
+						aria-label="Kropad's Telegram channel"
+						target="_blank"
+					>
+						Telegram
+					</a>
+					<a
+						href="https://github.com/Kropad"
+						class="hover:text-green-600 hover:underline"
+						aria-label="Kropad's GitHub profile"
+						target="_blank"
+					>
+						GitHub
+					</a>
+					<a
+						href="mailto:contact@kropad.com"
+						class="hover:text-green-600 hover:underline"
+						aria-label="Kropad's mail address"
+						target="_blank"
+					>
+						Mail
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
